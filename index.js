4b559bb55f96be1a89f7795e3f5922bba9831bab
Maaz Bobat <79587806+maazbobat@users.noreply.github.com>
2023-06-14 13:58:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+const PORT = 8080;
+
+app.listen(
+    PORT,
+    () => console.log(`its alive on http://localhost:${PORT}`)
+)
+
+app.get('/tshirt', (req,res)=>{
+    res.status(200).send({
+        tshirt:'👕',
+        size:'large'
+    })
+});
+
+app.post('/tshirt/:id')
